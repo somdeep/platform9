56e4a2871556324d0b0c7677c64b6ea933cc935c
somdeep <sd2988@columbia.edu>
2016-03-16 03:13:24 -0400

    Code cleaned, working smoothly

--- shell.c
+++ shell.c
@@ -7,33 +7,12 @@
 #include <stdio.h>  
 #include <unistd.h> 
 
-#include "try.c"
-// Shell command interpreter: Write a tool to implement bash-like functionality.
-// The tool should  
-// 1. Allow user to execute any commands, display output/errors
-// after command is executed.
-// 2. All user to chain multiple commands together. For
-// example -- ls -l | wc -l --> output of ls command is read by 'wc' and # of files
-// in current directory are printed as output.  
-// User should be able to start
-// interactive shell, execute multiple commands and press a key combination (e.g.
-// ^D) to exit the shell.  
-// locate *.txt | xargs grep 'abc' --> output of locate
-// command (file names) is sent as input to grep which then searches string 'abc'
-// in that file. The net result is that the command outputs lines with text 'abc'
-// from all .txt files in current directory
 
 static void sig_int(int);       
 
 int
 main(int argc,char * argv[])
 {
-         
-         // if(argc>2)
-         // {
-         //    printf("\nOnly single argument supported\n");
-         //    exit(0);
-         // }
         char    buf[MAXLINE];
        /* from apue.h */
         char    first[MAXLINE];
@@ -48,9 +27,8 @@ main(int argc,char * argv[])
         while(1)
         {    
         printf("\n%%");
-        //("%s",buf);
         gets(buf);
-        printf("%s\n",buf );
+        //printf("%s\n",buf );
         char *execArgs[20];
 
         int i=0;
@@ -81,17 +59,14 @@ main(int argc,char * argv[])
         }
 
         second[j]='\0';
-        printf("%s\n",first );
-        printf("%s\n",second );
+        // printf("%s\n",first );
+        // printf("%s\n",second );
        
         i=0;j=0;
 
         char* token = strtok(first, delimit);
         while (token) 
         {
-            printf("token in first: %s\n", token);
-            // delim=dup[token-first+strlen(token)];
-            // printf("delim : %c\n", delim);
             
             execArgs[i]=token;    
              token = strtok(NULL, delimit);
@@ -100,8 +75,8 @@ main(int argc,char * argv[])
         }
     
         execArgs[i]=NULL;
-        printf("%s\n",first );
-        printf("%s\n",second );
+        // printf("%s\n",first );
+        // printf("%s\n",second );
         
         i=0;
         token=NULL;
@@ -110,9 +85,6 @@ main(int argc,char * argv[])
         char *t = strtok(second, delimit);
         while (t) 
         {
-            printf("t: %s\n", t);
-            //delim=dup[token-second+strlen(token)];
-            //printf("delim : %c\n", delim);
             
             sec[i]=t;    
              t= strtok(NULL, delimit);
@@ -125,11 +97,7 @@ main(int argc,char * argv[])
         pipe(p);
 
 
-        // if(sec[0]!=NULL)
-        // printf("%s\n",sec[0] );
-        
-         // struct command cmd [] = { {execArgs}, {sec}};       
-         // return fork_pipes (2, cmd); 
+       
                 if ((pid=fork()) == 0) {      /* child */
                         // execvp("ls", argv);
                         
@@ -147,7 +115,7 @@ main(int argc,char * argv[])
 
                 if(sec[0])
                 {    
-                    printf("In second process : %s\n",sec[0]);
+                    
                 if (fork() == 0) {      /* second child */
                         // execvp("ls", argv);
                          
@@ -166,9 +134,7 @@ main(int argc,char * argv[])
                 close(p[1]);
                 if ((pid = waitpid(-1, &status, WNOHANG)) < 0)
                         err_sys("waitpid error");
-                // if(wait(&status)<0)
-                //     err_sys(" " );
-                //waitpid(pid, &status, 0);
+               
         
     
             }
